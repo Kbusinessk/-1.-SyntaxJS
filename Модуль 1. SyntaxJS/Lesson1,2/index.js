db8b2da50965b7bb14d332c374e2d17315f108a9
Karina <90905660+Kbusinessk@users.noreply.github.com>
2022-09-07 18:09:44 +1000

    Add files via upload

---
+++ Модуль 1. SyntaxJS/Lesson1,2/index.js
@@ -0,0 +1,23 @@
+//“JavaScript-код с созданием переменных”
+
+/*“Автор:”
+Karina Kaptola*/
+
+const myName = 'Karina'
+console.log('My Name:', myName)
+
+const mySurname = 'Kaptola'
+console.log('My Surname:', mySurname)
+
+let myFavoriteDrink = 'Mango Juice'
+console.log('My Favorite Drink:', myFavoriteDrink)
+
+let myFavoriteAnimal = 'Dog'
+console.log('My Favorite Animal', myFavoriteAnimal)
+
+let myFavoriteProgrammingLanguage = 'JavaScript'
+console.log('My Favorite Programming Language:', myFavoriteProgrammingLanguage)
+
+let country = 'Russia'
+country = 'Finland'
+console.log(country)
---
+++ Модуль 1. SyntaxJS/Lesson3,4/car.js
@@ -0,0 +1,24 @@
+const carName = 'Марка машины: Nissan Qashqai'
+alert(carName)
+
+let maxCarSpeed = 'Максимальная скорость: 190 km/ч'
+alert(maxCarSpeed)
+maxCarSpeed = 195
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+maxCarSpeed = 200
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+maxCarSpeed = 205
+console.log('У машины изменилась скорость', maxCarSpeed)
+
+let carOwner = 'Владелец машины: Alex'
+alert(carOwner)
+carOwner = 'Владельцем машины стал: Robert'
+console.log('У машины изменился владелец!', carOwner)
+
+carOwner = 'Владельцем машины стал: Max'
+console.log('У машины изменился владелец!', carOwner)
+
+carOwner = 'Владельцем машины стал: Jackob'
+console.log('У машины изменился владелец!', carOwner)
